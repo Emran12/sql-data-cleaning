af1628ab84b96ebb2dc516da8978333aa59dac78
Emran12 <emran6452@gmail.com>
2025-11-18 22:14:11 +0600

    Removed null and blank values

--- data-cleaning-project.sql
+++ data-cleaning-project.sql
@@ -142,7 +142,9 @@ ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` date; 
 
 SELECT *
-FROM layoffs_staging2; 
+FROM layoffs_staging2
+WHERE company = 'Airbnb'; 
+
 
 SELECT *
 FROM layoffs_staging2
@@ -152,6 +154,42 @@ AND percentage_laid_off IS NULL;
 SELECT * 
 FROM layoffs_staging2
 WHERE industry IS NULL 
-OR industry = ''
+OR industry = '';
 
+UPDATE layoffs_staging2
+SET industry = NULL 
+WHERE industry = ''; 
 
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2 
+ON t1.company = t2.company
+AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL 
+AND percentage_laid_off IS NULL;
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL 
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+SELECT *
+FROM layoffs_staging2;
